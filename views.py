c62a8475728bdc8060d39bdba1d5aaf4e42d198f
zj <1275423409@qq.com>
2023-11-17 16:13:39 +0800

    创建视图

--- views.py
+++ views.py
@@ -1,4 +1,7 @@
 from django.http import HttpResponse
+from django.shortcuts import rediect
 
 def index(request):
     return HttpResponse('index')
+def login(request):
+    return rediect('/index')
